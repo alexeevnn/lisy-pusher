852a783134a189f080a2d61d0477d8cd52c4f04b
alexeevnn <alexeevnn@gmail.com>
2025-05-18 15:55:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from flask import Flask, request
-from flask import Flask, request
 import requests
 import os
 
 app = Flask(__name__)
 
-# Telegram settings
-BOT_TOKEN = "7653342813:AAHwbbcVLJpqypiejgY5FsV9FQLrDeXVQOU"
-CHAT_ID = "1921885707"  # Замени на свой Telegram ID, если нужно
+# Получаем токен и чат из переменных окружения
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")  # можно задать в Render тоже
 
 @app.route("/", methods=["POST"])
 def handle_push():
     data = request.get_json()
     text = data.get("text", "Пустое сообщение")
+
+    if not BOT_TOKEN or not CHAT_ID:
+        return {"ok": False, "error": "Missing BOT_TOKEN or CHAT_ID"}, 500
 
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
@@ -27,7 +29,7 @@ def handle_push():
 
 @app.route("/", methods=["GET"])
 def health_check():
-    return "Lisy-pusher is live!"
+    return "Lisy-pusher is alive and safe!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
